9ca56ecc9baf483b66230f427f3421f284cb1afd
Yaroslav Berezin <yb2003work@gmail.com>
2021-09-17 00:28:54 +0300

    V1.5

--- src/ru/vsu/sc/berezin_y_a/Main.java
+++ src/ru/vsu/sc/berezin_y_a/Main.java
@@ -6,47 +6,77 @@ public class Main {
 
     public static void main(String[] args) {
 
-        compareBoxes();
+        printSomething("Enter sizes of the first box: ");
+        int sideX1 = readNum("X: ");
+        checkNum(sideX1);
+        int sideY1 = readNum("Y: ");
+        checkNum(sideY1);
+        int sideZ1 = readNum("Z: ");
+        checkNum(sideZ1);
+
+        printSomething("Enter sizes of the first box: ");
+        int sideX2 = readNum("X: ");
+        checkNum(sideX1);
+        int sideY2 = readNum("Y: ");
+        checkNum(sideY1);
+        int sideZ2 = readNum("Z: ");
+        checkNum(sideZ1);
+
+        printFinalAnswerOfATask(sideX1, sideY1, sideZ1, sideX2, sideY2, sideZ2);
 
     }
 
-    public static void compareBoxes() {
+    public enum states{
+        secondInFirst,
+        firstInSecond,
+        boxesAreDifferent;
+    }
 
-        System.out.println("Enter sizes of the first box: ");
-        System.out.print("X: ");
-        int x1 = readNum();
-        System.out.print("Y: ");
-        int y1 = readNum();
-        System.out.print("Z: ");
-        int z1 = readNum();
+    static void printSomething(String text) {
+        System.out.println(text);
+    }
 
-        System.out.println("Enter sizes of the second box: ");
-        System.out.print("X: ");
-        int x2 = readNum();
-        System.out.print("Y: ");
-        int y2 = readNum();
-        System.out.print("Z: ");
-        int z2 = readNum();
+    static int readNum(String text) {
+        System.out.print(text);
+        Scanner scanner = new Scanner(System.in);
+        return (scanner.nextInt());
+    }
 
+    static states checkPlacementOfBoxes(int x1, int y1, int z1, int x2, int y2, int z2) {
         if (((x1 > x2) && (y1 > y2) && (z1 > z2)) ||
                 ((x1 > x2) && (y1 > z2) && (z1 > y2)) ||
                 ((y1 > y2) && (x1 > z2) && (z1 > x2)) ||
                 ((z1 > z2) && (x1 > y2) && (y1 > x2))) {
-            System.out.println("The second box is placed in the first");
+            return (states.secondInFirst);
         } else if (((x2 > x1) && (y2 > y1) && (z2 > z1)) ||
                 ((x2 > x1) && (y2 > z1) && (z2 > y1)) ||
                 ((y2 > y1) && (x2 > z1) && (z2 > x1)) ||
                 ((z2 > z1) && (x2 > y1) && (y2 > x1))) {
-            System.out.println("The first box is placed in the second");
+            return (states.firstInSecond);
         } else {
-            System.out.println("It's impossible to place one box in another");
+            return (states.boxesAreDifferent);
         }
-
     }
 
-    public static int readNum() {
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+    static String getStringKnowingState(states state) {
+        if (state == states.secondInFirst) {
+            return ("The second box is in the first");
+        } else if (state == states.firstInSecond) {
+            return ("The first box is in the second");
+        } else {
+            return ("Boxes are different");
+        }
+    }
+
+    static void checkNum(int num) {
+        if (num < 0) {
+            System.out.println("Error, the side can't be less than 0");
+            System.exit(0);
+        }
+    }
+
+    static void printFinalAnswerOfATask(int x1, int y1, int z1, int x2, int y2, int z2) {
+        printSomething(getStringKnowingState(checkPlacementOfBoxes(x1, y1, z1, x2, y2, z2)));
     }
 
 }
