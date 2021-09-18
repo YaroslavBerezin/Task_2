83b8a48de01522abdfac0db00cd3c926b48c85f6
Yaroslav Berezin <yb2003work@gmail.com>
2021-09-18 11:37:01 +0300

    V1.7

--- src/ru/vsu/sc/berezin_y_a/Main.java
+++ src/ru/vsu/sc/berezin_y_a/Main.java
@@ -22,27 +22,27 @@ public class Main {
         int sideZ2 = readNum("Z: ");
         checkNum(sideZ1);
 
-        printFinalAnswerOfATask(sideX1, sideY1, sideZ1, sideX2, sideY2, sideZ2);
+        getStringKnowingState(checkPlacementOfBoxes(sideX1, sideY1, sideZ1, sideX2, sideY2, sideZ2));
 
     }
 
-    public enum states{
+    private enum states{
         SECONDINFIRST,
         FIRSTINSECOND,
         BOXESAREDIFFERENT;
     }
 
-    static void printSomething(String text) {
+    private static void printSomething(String text) {
         System.out.println(text);
     }
 
-    static int readNum(String text) {
+    private static int readNum(String text) {
         System.out.print(text);
         Scanner scanner = new Scanner(System.in);
         return (scanner.nextInt());
     }
 
-    static states checkPlacementOfBoxes(int x1, int y1, int z1, int x2, int y2, int z2) {
+    private static states checkPlacementOfBoxes(int x1, int y1, int z1, int x2, int y2, int z2) {
         if (((x1 > x2) && (y1 > y2) && (z1 > z2)) ||
                 ((x1 > x2) && (y1 > z2) && (z1 > y2)) ||
                 ((y1 > y2) && (x1 > z2) && (z1 > x2)) ||
@@ -58,25 +58,22 @@ public class Main {
         }
     }
 
-    static String getStringKnowingState(states state) {
-        if (state == states.SECONDINFIRST) {
-            return ("The second box is in the first");
-        } else if (state == states.FIRSTINSECOND) {
-            return ("The first box is in the second");
-        } else {
-            return ("Boxes are different");
+    private static void getStringKnowingState(states state) {
+        switch (state) {
+            case SECONDINFIRST:
+                System.out.println("The second box is in the first");
+            case FIRSTINSECOND:
+                System.out.println("The first box is in the second");
+            case BOXESAREDIFFERENT:
+                System.out.println("Boxes are different");
         }
     }
 
-    static void checkNum(int num) {
+    private static void checkNum(int num) {
         if (num < 0) {
             System.out.println("Error, the side can't be less than 0");
             System.exit(0);
         }
-    }
-
-    static void printFinalAnswerOfATask(int x1, int y1, int z1, int x2, int y2, int z2) {
-        printSomething(getStringKnowingState(checkPlacementOfBoxes(x1, y1, z1, x2, y2, z2)));
     }
 
 }
